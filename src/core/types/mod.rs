b3aca388d5c0404307078e18ffa913b17b5a3e90
mns03 <rainstorm9670@gmail.com>
2026-08-31 14:18:59 +0900

    feat(types): add position storage types

--- src/core/types/mod.rs
+++ src/core/types/mod.rs
@@ -6,6 +6,8 @@ pub(crate) mod nonzero;
 pub mod params;
 pub mod pool_key;
 pub(crate) mod pool_state;
+#[allow(dead_code)]
+pub(crate) mod position;
 pub(crate) mod sqrt_price;
 pub(crate) mod tick;
 pub(crate) mod tick_slab_indexer;
